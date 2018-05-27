01c32107db907647e57e914901135d95730cbcd8
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-05-27 16:21:04 -0700

    genexp vs listcomp

--- ch02_sequences/list_comprehensions.py
+++ ch02_sequences/list_comprehensions.py
@@ -31,3 +31,53 @@ def map_filter_example():
     symbols = '$%^&*('
     codes = [ord(x) for x in symbols if ord(x) > 127]
     print(codes)
+
+
+def cartesian_product_example():
+
+    sizes = ['S', 'M', 'L', 'XL']
+    colors = ['black', 'pink', 'yellow']
+
+    # naive approach
+    tshirts = []
+    for size in sizes:
+        for color in colors:
+            tshirts.append((size, color))
+    print(tshirts)
+
+    # comprehension
+    tshirts = [(size, color) for size in sizes
+                             for color in colors]
+    print(tshirts)
+
+
+def genexps_example():
+    '''
+    genexps save memory because they are kind of lazy
+    and yeild items on demand one-by-one, using the iterator
+    protocol instead of building the whole list as listcomps do.
+    '''
+
+    symbols = '$%&*('
+    codes = tuple(ord(symbol) for symbol in symbols)
+    print(codes)
+
+    import array
+    ar = array.array('I', (ord(symbol) for symbol in symbols))
+    print(ar)
+
+    colors = ['black', 'red', 'green']
+    sizes = ['S', 'M', 'XL']
+    for tshirt in ("%s %s" % (size, color) for size in sizes
+                                           for color in colors):
+        print(tshirt)
+
+
+if __name__ == '__main__':
+    cartesian_product_example()
+
+    readability_example()
+
+    map_filter_example()
+
+    genexps_example()
