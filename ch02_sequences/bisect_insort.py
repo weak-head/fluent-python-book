6f26f12902e29d88b599a91971ca8d001cee336d
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-05-29 23:17:28 -0700

    bisect for table lookup

--- ch02_sequences/bisect_insort.py
+++ ch02_sequences/bisect_insort.py
@@ -19,6 +19,18 @@ def bisect_demo(bisect_fn):
         print(ROW_FORMAT.format(needle, index, offset))
 
 
+def bisect_table_lookup():
+    '''
+    Juicy example of bisect usage for table lookup.
+    '''
+
+    def to_grade(score, breakpoints=[60, 70, 80, 90], scores='FDCBA'):
+        score_index = bisect.bisect(breakpoints, score)
+        return scores[score_index]
+
+    [print(to_grade(score)) for score in [33, 90, 74, 82, 66, 95, 99, 70, 60]]
+
+
 if __name__ == '__main__':
 
     print('bisect right:')
@@ -29,3 +41,6 @@ if __name__ == '__main__':
     print('bisect left:')
     print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
     bisect_demo(bisect.bisect_left)
+
+    print(); print()
+    bisect_table_lookup()
