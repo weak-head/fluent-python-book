b48ec053acc46550908bcf292e2a577746c0503a
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-05-29 23:33:11 -0700

    insort

--- ch02_sequences/bisect_insort.py
+++ ch02_sequences/bisect_insort.py
@@ -1,6 +1,7 @@
 #!/python/bin
 
 import bisect
+import random
 
 HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
 NEEDLES  = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
@@ -31,6 +32,22 @@ def bisect_table_lookup():
     [print(to_grade(score)) for score in [33, 90, 74, 82, 66, 95, 99, 70, 60]]
 
 
+def insort_example():
+    '''
+    insort from bisect module helps us to
+    preserver sorted invariant of a list.
+    '''
+
+    random.seed(1729)
+    size = 7
+    the_list = []
+
+    for i in range(size):
+        rn = random.randrange(size * 2)
+        bisect.insort(the_list, rn)
+        print('%2d ->' % rn, the_list)
+
+
 if __name__ == '__main__':
 
     print('bisect right:')
@@ -44,3 +61,6 @@ if __name__ == '__main__':
 
     print(); print()
     bisect_table_lookup()
+
+    print(); print()
+    insort_example()
