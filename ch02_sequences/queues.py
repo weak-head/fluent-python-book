5bc5f6971dce3e27dc3fbb9f2b90731b3b0f6780
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-05-31 22:45:10 -0700

    deque

---
+++ ch02_sequences/queues.py
@@ -0,0 +1,43 @@
+#!/bin/python
+
+from collections import deque
+
+def deque_example():
+
+    dq = deque(range(10), maxlen=10)
+    print('dq:')
+    print(dq)
+    print()
+
+    dq.append(55)
+    print('append 55')
+    print(dq)
+    print()
+
+    dq.appendleft(-55)
+    print('appendleft -55')
+    print(dq)
+    print()
+
+    dq.rotate(3)
+    print('rotate 3')
+    print(dq)
+    print()
+
+    dq.rotate(-6)
+    print('rotate -6')
+    print(dq)
+    print()
+
+    dq.extend([77, 88, 99])
+    print('extend 77 88 99')
+    print(dq)
+    print()
+
+    dq.extendleft([-77, -88, -99])
+    print('extendleft -77 -88 -99')
+    print(dq)
+    print()
+
+if __name__ == '__main__':
+    deque_example()
