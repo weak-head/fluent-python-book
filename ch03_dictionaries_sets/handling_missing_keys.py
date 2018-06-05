0f33114fa3dee320c0435c8b4e8ee14635f4dc9a
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-04 21:50:56 -0700

    elegant solution of handling missing keys using collections.defaultdict

--- ch03_dictionaries_sets/handling_missing_keys.py
+++ ch03_dictionaries_sets/handling_missing_keys.py
@@ -56,8 +56,41 @@ def setdefault_dict():
         print(word, len(index[word]), index[word])
 
 
+def defaultdict_beauty():
+    '''
+    The elegant solution using defaultdict
+    from collections
+    '''
+
+    import sys
+    import re
+    import collections
+
+    WORD_RE = re.compile(r'\w+')
+
+    # `list` is basically the callable
+    # that is used to produce a default value.
+    # in this case the default value is empty list.
+    index = collections.defaultdict(list)
+
+    with open('handling_missing_keys.py', encoding='utf-8') as fp:
+        for line_no, line in enumerate(fp, 1):
+            for match in WORD_RE.finditer(line):
+                word = match.group()
+                col_no = match.start() + 1
+                location = (line_no, col_no)
+
+                index[word].append(location)
+
+    for word in sorted(index, key=str.upper):
+        print(word, len(index[word]), index[word])
+
+
 if __name__ == '__main__':
     naive_dict_get()
 
     print(); print()
     setdefault_dict()
+
+    print(); print()
+    defaultdict_beauty()
