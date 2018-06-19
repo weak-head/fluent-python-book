045bb4515cb2311ec662ee9956c06baec1336638
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-18 17:46:27 -0700

    deque for moving average

--- ch03_dictionaries_sets/playground/deque_usecases.py
+++ ch03_dictionaries_sets/playground/deque_usecases.py
@@ -1,4 +1,5 @@
 from collections import deque
+from itertools   import  islice
 
 def tail_filter():
     """deque could be used as unix-like tail."""
@@ -8,6 +9,24 @@ def tail_filter():
         numbered = ['{:0>2d} {}'.format(n, s) for n, s in last_five]
         print(''.join(numbered))
 
+def moving_average(iterable, n=3):
+    it = iter(iterable)
+
+    # first n-1 items of the iterable
+    avgr = deque(islice(it, n-1))
+    avgr.appendleft(0)
+    s = sum(avgr)
+
+    for i in it:
+        s -= avgr.popleft()
+
+        avgr.append(i)
+        s += i
+
+        yield s / n
+
+
 
 if __name__ == '__main__':
     tail_filter()
+    print(list(moving_average([10, 20, 30, 40, 50, 60, 70])))
